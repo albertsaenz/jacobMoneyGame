2ab6bc273d7cd304978717e5dc397a9773acadeb
Albert Romero Saenz <sheik.albert@gmail.com>
2020-06-25 18:52:20 -0500

    aa
    a

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -90,7 +90,7 @@ class Play extends Phaser.Scene {
         /*evento de teclado barra espaciadora que inicia el juego*/
         
             timecreateCovid = this.time.addEvent({
-                delay: 300,
+                delay: 400,
                 callback: this.covide,
                 callbackScope: this,
                 // repeat:2
